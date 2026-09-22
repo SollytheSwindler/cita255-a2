e8d971b47029d1bd6d60ac75bad96b910fb5bb05
SollytheSwindler <solomonrayland@gmail.com>
2026-09-22 16:15:01 -0400

    Created Assignment 2

---
+++ CITA255-a2/CITA255-a2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CITA255_a2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CITA255-a2/CITA255-a2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CITA255_a2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
