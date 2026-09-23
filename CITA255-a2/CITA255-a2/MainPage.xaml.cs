3acd6ded0f5b2e272090280f20ff3e5f6e011747
SollytheSwindler <solomonrayland@gmail.com>
2026-09-23 16:18:20 -0400

    Update for Assignment 2

--- CITA255-a2/CITA255-a2/MainPage.xaml.cs
+++ CITA255-a2/CITA255-a2/MainPage.xaml.cs
@@ -2,23 +2,17 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        List<string> game = new List<string> {"Elden Ring", "Clair Obscur: Expedition 33", "Bloodborne", "Lies of P",
+        "Fatal Fury: City of the Wolves", "Granblue Fantasy Versus: Rising", "The Last Faith", "Tekken 8",
+        "The First Berserker Khazan", "Tales of Arise", "Devil May Cry 5 Special Edition", "The King of Fighters XV",
+        "Virtua Fighter 5 REVO World Stage", "Final Fantasy XVI"};
+        List<int> hours = new List<int> {261, 37, 169, 225, 31, 24, 19, 220, 127, 0, 189, 21, 16, 89};
 
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            gameList.ItemsSource = game;
+            hoursList.ItemsSource = hours;
         }
     }
 }
---
+++ test/test/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace test
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        private int games = int.Parse(gameEntry.Text);
+        private double hours = double.Parse(hourEntry.Text);
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
